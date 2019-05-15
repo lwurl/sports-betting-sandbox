ce559ecf206b1be8d96a6d1b4f30052bb6e7f518
Luke Wurl <lwurl@nd.edu>
2019-05-15 16:27:03 -0400

    headerbar template complete

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import { withStyles, createStyles, WithStyles } from "@material-ui/core/styles";
+
 import styled from "styled-components";
 
-function HeaderBar() {
+function HeaderBar(props: WithStyles) {
+  const { classes } = props;
+
   return (
     <HorizontalExpander>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="inherit" className={classes.appTitle}>
             Sports Betting Sandbox
           </Typography>
+          <Button className={classes.headerButton}>Leaderboard</Button>
+          <Button className={classes.headerButton}>Wager</Button>
+          <Avatar className={classes.avatar}>LW</Avatar>
         </Toolbar>
       </AppBar>
     </HorizontalExpander>
@@ -22,4 +32,16 @@ const HorizontalExpander = styled.div`
   flexgrow: 1;
 `;
 
-export default HeaderBar;
+const styles = createStyles({
+  appTitle: {
+    flex: 1
+  },
+  headerButton: {
+    color: "white"
+  },
+  avatar: {
+    margin: 10
+  }
+});
+
+export default withStyles(styles)(HeaderBar);
